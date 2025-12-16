9a9add4af4374e7565795ce5dd7626a44b8803e5
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-12-16 10:31:04 +0000

    fix: export all components from schedule section

---
+++ audit-management-system/src/app/dashboard/schedule/components/index.ts
@@ -0,0 +1,9 @@
+export { ScheduleHeader } from "./ScheduleHeader";
+export { ReferenceSection } from "./ReferenceSection";
+export { CompanySection } from "./CompanySection";
+export { AuditeeSection } from "./AuditeeSection";
+export type { Auditee } from "./AuditeeSection";
+export { AuditorSection } from "./AuditorSection";
+export { PurposeScopeSection } from "./PurposeScopeSection";
+export { PrimaryActions } from "./PrimaryActions";
+export { SecondaryActions } from "./SecondaryActions";
